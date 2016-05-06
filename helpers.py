a4b4890961bda1a573ee7c5176a6fe2ffb20e851
roybayot.in.lab <roybayot@gmail.com>
2016-05-06 20:50:26 +0100

    add Parser from http://stackoverflow.com/questions/10508487/xml-parsing-in-python-using-elementtree

--- helpers.py
+++ helpers.py
@@ -13,10 +13,37 @@ from scipy import stats
 
 import re
 
+import xml.etree.ElementTree as ET
+import sys
+import re
+
+
 reload(sys)
 #sys.setdefaultencoding("ISO-8859-1")
 sys.setdefaultencoding("UTF-8")
 
+class MyXMLParser(ET.XMLParser):
+
+    rx = re.compile("&#([0-9]+);|&#x([0-9a-fA-F]+);")
+
+    def feed(self,data):
+        mydata = data
+        m = self.rx.search(data)
+        if m is not None:
+            target = m.group(1)
+            if target:
+                num = int(target)
+            else:
+                num = int(m.group(2), 16)
+            if not(num in (0x9, 0xA, 0xD) or 0x20 <= num <= 0xD7FF
+                   or 0xE000 <= num <= 0xFFFD or 0x10000 <= num <= 0x10FFFF):
+                # is invalid xml character, cut it out of the stream
+                print 'removing %s' % m.group()
+                mstart, mend = m.span()
+                mydata = data[:mstart] + data[mend:]
+        else:
+            mydata = data
+        super(MyXMLParser,self).feed(mydata)
 
 def clean_text(raw_text):
     review_text = BeautifulSoup(raw_text).get_text()
